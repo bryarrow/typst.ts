b1118729334aaf698bccb169f33aaf50a0b78584
bryarrow <220811010104@qhu.edu.cn>
2025-09-21 23:13:17 +0800

    feat: add a demo for TypstDocument

--- packages/typst.vue3/dev/App.vue
+++ packages/typst.vue3/dev/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import Typst from '../src/Typst.vue';
+import TypstDocument from '../src/TypstDocument.vue';
+import { ref } from 'vue';
+import { $typst } from '@myriaddreamin/typst.ts';
 
 const sourceCode = '= Hello World!\nThis is a typst document compiled in browser.';
+
+let artifact = ref(new Uint8Array(0));
+$typst.vector({ mainContent: sourceCode }).then(value => {
+  artifact.value = value;
+});
 </script>
 
 <template>
-  <Typst v-bind:content="sourceCode" />
+  <Typst :content="sourceCode" />
+  <TypstDocument :artifact="artifact" />
 </template>
 
 <style lang="css"></style>
